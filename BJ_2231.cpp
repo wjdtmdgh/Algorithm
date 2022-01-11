f29080249a87c7769ec83d541abc374560e38e34
wjdtmdgh <72695452+wjdtmdgh@users.noreply.github.com>
2022-01-11 14:10:10 +0900

    Create BJ_2231.cpp

---
+++ BJ_2231.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int num;
+	int sum;
+	int part;
+	cin >> num;
+	for (int i = 1; i < num; ++i) {
+		sum = i;
+		part = i;
+		while (part) {
+			sum += part % 10;
+			part /= 10;
+		}
+		if (num == sum) {
+			cout << i << endl;
+			return 0;
+		}
+	}
+ 
+	cout << "0" << endl;
+ 
+	return 0;
+}
